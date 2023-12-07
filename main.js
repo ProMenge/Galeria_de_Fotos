e11192f6c3385b5556e89294ca23c7eac32efd6f
Fred Menge <fredericoavari@gmail.com>
2023-12-07 11:41:21 -0300

    Galeria de Fotos - adição dos arquivos

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    //BOTAO IMAGEM +
+    $('header button').click(function(){
+        $('form').slideDown();
+
+    })
+
+    //BOTÃO DE CANCELAR
+    $('#botao-cancelar').click(function(){
+
+        $('form').slideUp();
+
+    })
+
+    //BOTAO DE ADICIONAR A IMAGEM
+    $('form').on('submit', function(e){
+
+        e.preventDefault();
+        const enderecoNovaImagem = $('#endereço-imagem-nova').val();
+        console.log(enderecoNovaImagem);
+        const novoItem = $('<li style=" display: none"></li>');
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+
+        $(` 
+            <div class="overlay-imagem-link"> 
+                <a href="${enderecoNovaImagem}" target="_blank" title="Ver Imagem Inteira">
+                    Ver Imagem Inteira
+                </a>
+            </div>
+            
+        `).appendTo(novoItem);
+
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn();
+
+        $('#endereço-imagem-nova').val(' ');
+    })
+
+})
